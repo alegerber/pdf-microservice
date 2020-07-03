56b27383fb77b256a57c05a71b7709f197563255
Alexander Gerber <alexander.gerber@check24.de>
2020-07-03 23:38:35 +0200

    add test

---
+++ tests/Application/Actions/Pdf/GenerateActionTest.php
@@ -0,0 +1,122 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Application\Actions\Pdf;
+
+use App\Application\Actions\Pdf\GenerateAction;
+use App\Application\Validator\GenerateValidator;
+use App\Application\Validator\ValidationException;
+use App\Domain\Pdf\Pdf;
+use PHPUnit\Framework\MockObject\Rule\InvokedCount;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Psr7\Response;
+use Tests\TestCase;
+
+class GenerateActionTest extends TestCase
+{
+
+    /**
+     * @param array $formData
+     * @param InvokedCount $setPaper
+     * @dataProvider providerTestAction
+     */
+    public function testAction(array $formData, InvokedCount $setPaper)
+    {
+        $pdf               = $this->createMock(Pdf::class);
+        $generateValidator = $this->createMock(GenerateValidator::class);
+
+        $pdf->expects($this->once())->method('loadHtml');
+        $pdf->expects($setPaper)->method('setPaper');
+
+        $generateAction = $this->createPartialMock(GenerateAction::class, [
+            'getPdf',
+            'getGenerateValidator',
+            'getFormData',
+            'respondWithData',
+        ]);
+
+        $generateAction->method('getPdf')->willReturn($pdf);
+        $generateAction->method('getGenerateValidator')->willReturn($generateValidator);
+        $generateAction->method('getFormData')->willReturn($formData);
+        $generateAction->method('respondWithData')->willReturn(new Response());
+
+
+        $this->assertInstanceOf(ResponseInterface::class, $generateAction->action());
+    }
+
+
+    /**
+     * @return array
+     */
+    public function providerTestAction(): array
+    {
+        return [
+            'only_html' => [
+                'formData' => ['html' => 'test'],
+                'setPaper' => $this->never()
+            ],
+            'html_paper' => [
+                'formData' => [
+                    'html' => 'test',
+                    'paper' => [
+                        'size' => 'A4',
+                        'orientation' => 'landscape'
+                        ]
+                ],
+                'setPaper' => $this->once()
+            ],
+        ];
+    }
+
+    /**
+     * @param array  $formData
+     * @param string $exceptionMessage
+     * @dataProvider providerTestActionException
+     */
+    public function testActionExcpetion(array $formData, string $exceptionMessage)
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        $pdf               = $this->createMock(Pdf::class);
+        $generateValidator = new GenerateValidator();
+
+        $generateAction = $this->createPartialMock(GenerateAction::class, [
+            'getPdf',
+            'getGenerateValidator',
+            'getFormData',
+            'respondWithData',
+        ]);
+
+        $generateAction->method('getPdf')->willReturn($pdf);
+        $generateAction->method('getGenerateValidator')->willReturn($generateValidator);
+        $generateAction->method('getFormData')->willReturn($formData);
+        $generateAction->method('respondWithData')->willReturn(new Response());
+
+        $this->assertInstanceOf(ResponseInterface::class, $generateAction->action());
+    }
+
+
+    /**
+     * @return array
+     */
+    public function providerTestActionException(): array
+    {
+        return [
+            'only_html' => [
+                'formData' => ['hmtl' => 'paper'],
+                'exceptionMessage' => 'validation fails for \'html\''
+            ],
+            'html_paper' => [
+                'formData' => [
+                    'html' => 'test',
+                    'paper' => [
+                        'size' => 'A4',
+                        ]
+                ],
+                'exceptionMessage' => 'validation fails for \'paper\''
+            ],
+        ];
+    }
+
+}
